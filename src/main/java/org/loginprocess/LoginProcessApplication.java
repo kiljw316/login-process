1c0001e60bbd668d63c81de53b5fdfd389a01d54
kiljaewon <kiljw316@gmail.com>
2024-03-20 00:27:38 +0900

    initial commit

---
+++ src/main/java/org/loginprocess/LoginProcessApplication.java
@@ -0,0 +1,13 @@
+package org.loginprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginProcessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/loginprocess/LoginProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package org.loginprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProcessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
